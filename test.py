c44ed2e2e795e7a1937ef195bf4fbcaaf8858be4
Haplocorn <haplocorn@gmail.com>
2019-03-21 15:59:26 +0100

    Made a program that trains a couple of ml models and prints some of their stats on performance

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import sys
 
 import pandas as pd
+from sklearn import metrics, svm
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler, LabelBinarizer
+from sklearn.tree import DecisionTreeClassifier
 
-# import data
-data = pd.read_csv('./train.csv')
-
+# constants
 relevant_features = [
     'Elevation',
     'Horizontal_Distance_To_Roadways',
@@ -22,22 +26,110 @@ relevant_features = [
 
 target_feature = ['Cover_Type']
 
-# cross-validation
-random_forrest = RandomForestClassifier(n_estimators=100)
-scores = cross_val_score(random_forrest, data[relevant_features], data[target_feature].values.ravel(), cv=10)
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+cols_to_norm = [
+    "Elevation",
+    "Aspect",
+    "Slope",
+    "Horizontal_Distance_To_Hydrology",
+    "Vertical_Distance_To_Hydrology",
+    "Horizontal_Distance_To_Roadways",
+    "Hillshade_9am",
+    "Hillshade_3pm",
+    "Hillshade_Noon",
+    "Horizontal_Distance_To_Fire_Points"]
 
 
-# Used to identify important features
-# importance = random_forrest.feature_importances_
-# feature_names = list(data.columns.values[1:56])
+# A function used for getting the AUC when having multiple features
+def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
+    lb = LabelBinarizer()
+    lb.fit(y_test)
+
+    y_test = lb.transform(y_test)
+    y_pred = lb.transform(y_pred)
+
+    return roc_auc_score(y_test, y_pred, average=average)
+
+
+# import and split data
+data = pd.read_csv('./train.csv')
+train, test = train_test_split(data, test_size=0.2)
+
+# normalize the data of certain features
+scaler = StandardScaler().fit(data[cols_to_norm])
+train[cols_to_norm] = scaler.transform(train[cols_to_norm])
+test[cols_to_norm] = scaler.transform(test[cols_to_norm])
+
+# split the training data into feautures and target
+X = train[relevant_features]
+y = train[target_feature].values.ravel()
+
+
 #
-# feature_importance = list(zip(feature_names, importance))
-# feature_importance_sorted = sorted(feature_importance, key=lambda tup: tup[1], reverse=True)
+# Training and printing the results of multiple models using cross validation (10 folds)
 #
-# print("Importance features sorted")
-#
-# for el in feature_importance_sorted:
-#     print('%-35s %-21s' % (str(el[0]), str(el[1])))
+
+# Random forrest (n = 10, 30, 50, 70, 90)
+for i in range(10, 100, 20):
+    rfc = RandomForestClassifier(n_estimators=i)
+    rfc.fit(X.values, y)
+    scores = cross_val_score(rfc, train[relevant_features], train[target_feature].values.ravel(), cv=10)
+    print("rfc n=" + str(i) + " mean: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    y_pred = rfc.predict(test[relevant_features])
+    y_true = test[target_feature]
+
+    print("rfc n=" + str(i) + ", accuracy: " + str(metrics.accuracy_score(y_pred, y_true)))
+    print("rfc n=" + str(i) + ", precision: " + str(multiclass_roc_auc_score(y_pred, y_true)))
+
+
+# Decision Tree
+dtc = DecisionTreeClassifier()
+dtc.fit(X.values, y)
+scores = cross_val_score(dtc, train[relevant_features], train[target_feature].values.ravel(), cv=10)
+print("dtc mean: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+y_pred = dtc.predict(test[relevant_features])
+y_true = test[target_feature]
+
+print("dtc accuracy: " + str(metrics.accuracy_score(y_pred, y_true)))
+print("dtc precision: " + str(multiclass_roc_auc_score(y_pred, y_true)))
+
+# Gaussian Naive Bayes
+gnb = GaussianNB()
+gnb.fit(X.values, y)
+scores = cross_val_score(gnb, train[relevant_features], train[target_feature].values.ravel(), cv=10)
+print("gnb mean: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+y_pred = gnb.predict(test[relevant_features])
+y_true = test[target_feature]
+
+print("gnb accuracy: " + str(metrics.accuracy_score(y_pred, y_true)))
+print("gnb precision: " + str(multiclass_roc_auc_score(y_pred, y_true)))
+
+# Support Vector Machine (1 vs rest)
+SVC = svm.SVC(decision_function_shape='ovr', gamma='auto')
+SVC.fit(X.values, y)
+print("support vectors: " + str(len(SVC.support_vectors_)))
+scores = cross_val_score(SVC, train[relevant_features], train[target_feature].values.ravel(), cv=10)
+print("SVC 1vAll mean: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+y_pred = SVC.predict(test[relevant_features])
+y_true = test[target_feature]
+
+print("SVC 1vAll accuracy: " + str(metrics.accuracy_score(y_pred, y_true)))
+print("SVC 1vAll precision: " + str(multiclass_roc_auc_score(y_pred, y_true)))
+
+# KN Neighbour
+for i in range(1, 20):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X.values, y)
+    scores = cross_val_score(knn, train[relevant_features], train[target_feature].values.ravel(), cv=10)
+    print("KNN n=" + str(i) + " mean: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    y_pred = knn.predict(test[relevant_features])
+    y_true = test[target_feature]
+
+    print("KNN n=" + str(i) + ", accuracy: " + str(metrics.accuracy_score(y_pred, y_true)))
+    print("KNN n=" + str(i) + ", precision: " + str(multiclass_roc_auc_score(y_pred, y_true)))
 
 sys.exit(0)
